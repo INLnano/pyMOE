5f5726d81a697a0ae0a5d4040b20f8c9b152295f
cunhaJ <forsolar.cunha@gmail.com>
2021-12-10 16:36:31 +0000

    dither with instances

--- pyMOE/export.py
+++ pyMOE/export.py
@@ -245,11 +245,108 @@ def grayim2gds_writer(infile, outfile, pixelx, pixely, cellname, level, layer=0,
 
     print("Exported the image file "+str(infile) + " into " + str(outfile))
 
+def grayim2gds_writer_klops(infile,  output_filename , pixelx, pixely, cellname, level, layer=0, datatype=0 , verbose=False):
+    """
+    (void) Transforms one image (converted to grayscale) into a gds, using cv2 for reading the image
+    by default adds the image to (layer, datatype) = (0,0)
+    'infile'  = input IMAGE file (on extension that cv2 can read ), e.g. "image.png"
+    'output_filename' = intermediate GDS file
+    'pixelx'  = pixel size in x, in um 
+    'pixely'  = pixel size in y, in um 
+    'cellname'= string with cellname, e.g. "TOP"
+    'level'   = int from 0 to 255 (0 = black, 255=white) , pixels gray value to be passed to GDS 
 
+    optional:
+    'layer' = gray level, defaults to 0 
+    'datatype' = gds datatype, defaults to 0 
+    'verbose' defaults to False, if True prints 
 
+    """
+    
+    import cv2
+    import gdspy 
+    import numpy as np
+    
+    img = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
+    
+    if img is not None: 
+        print("Sucessfully imported img!")
+        
+    h,w = img.shape 
+    print(h)
+    print(w) 
+    
+    nmx = w
+    nmy = h
+    
+    harray = np.arange(0,h+1,nmy)
+    warray = np.arange(0,w+1,nmx)
+    #print(harray)
+    
+    lib = gdspy.GdsLibrary()
+    gdspy.current_library = gdspy.GdsLibrary() 
 
-###OTHERS 
+    outfile="image.gds"
+    writer = gdspy.GdsWriter(outfile,unit=1.0e-6,precision=1.0e-9)
+    cell = lib.new_cell(cellname)
+    
+    i=0 
+    j=0 
+    
+    rect = gdspy.Rectangle((pixelx*j,-pixely*i),(pixelx*(j+1), -pixely*(i+1)), layer, datatype)                   
+    cell.add(rect)        
+    writer.write_cell(cell)
+    writer.close()
+    
+    print("Exported the image file "+str(infile) + " into " + str(outfile))
+    
+    #####--------------------------------
+    print("Starting making instances")
+    
+    import pya
+    
+    layout = pya.Layout()
+    
+    #input_filename = "fresnel_phase_mask_newlayers.gds"
+    cell_name = "TOP" #name of the cell for instance array cannot be the same as the input gds filename
 
+    #create cell at top 
+    top = layout.create_cell(cell_name)
 
+    #gds files to read (could also be a list)
+    gds_files = [outfile]
 
+    for file in gds_files:
+        layout.read(file) #read the files
 
+        for top_cll in layout.top_cells():
+            if (top_cll.name != cell_name): #Don't insert in the top_cell
+                cell_index = top_cll.cell_index()
+
+                for hn, hi in enumerate(harray):
+                    if hn == (len(harray)-1):
+                        break
+                    #print(hi)
+                    for hw, wi in enumerate(warray):
+                        if hw == (len(warray)-1): 
+                            break   
+                        #print(wi)
+
+                        for i in np.arange(hi,hi+nmy):
+                            if verbose == True: 
+                                print(i/h)
+                            
+                            for j in np.arange(wi, wi+nmx):
+                                #print(j/w)
+                                #here we can also think of selectin pixels at a certain level only
+                                #and creating a GDS from a grayscale image 
+                                if img[i][j] == int(level):
+                                    new_instance = pya.CellInstArray( cell_index, pya.Trans(pya.Vector(int(j*1000),int(i*1000))), pya.Vector(0, 0), pya.Vector(0, 0), 0, 0)
+                                    top.insert( new_instance ) #insert the cell in the array
+            else:
+                if cellname==cell_name:
+                    print("Cell names seem to be the same. Proceed with caution, the final file might not be complete.")
+                
+    #write to gds
+    layout.write(output_filename)
+    print("Done")
