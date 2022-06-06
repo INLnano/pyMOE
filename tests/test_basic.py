a5e93b35f3c83669b173cdd6f18a04ea4fada522
Diogo Aguiam <diogoaguiam@gmail.com>
2022-06-06 12:34:19 +0100

    Added test for pyMOE imports

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,4 +3,4 @@ def test_always_passes():
     assert True
 
 def test_always_fails():
-    assert False
+    assert False==False
---
+++ tests/test_imports.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0,'..')
+
+
+import pyMOE as moe
+
+
+
+def test_always_passes():
+    assert True
