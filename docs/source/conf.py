4474d5f9675221ef7abd33ed88ec9aa8b4b96c5a
cunhaJ <forsolar.cunha@gmail.com>
2023-09-13 11:05:35 +0100

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,13 +12,16 @@ author = 'J. Cunha, D. E. Aguiam'
 release = 'v1.3'
 import os               # line 13
 import sys              # line 14
+import sphinx_rtd_theme
+
+
 sys.path.insert(0, os.path.abspath('../pyMOE/'))
 sys.path.insert(0, os.path.abspath('..')) 
 sys.path.insert(0, os.path.abspath('../..'))  
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 #pdf_documents = [('index', u'rst2pdf', u'Sample rst2pdf doc', u'Your Name'),]
 
@@ -30,5 +33,8 @@ autoclass_content = 'both'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'   
-html_static_path = ['_static']
+#html_theme = 'sphinx_rtd_theme'   
+#html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
