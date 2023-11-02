e6aeb7ad57abf5b6f293bef811f7301ac265e29b
cunhaJ <forsolar.cunha@gmail.com>
2023-11-02 20:11:16 +0000

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys              # line 14
 import sphinx_rtd_theme
 
 
-sys.path.insert(0, os.path.abspath('../pyMOE/'))
+sys.path.insert(0, os.path.abspath('../../pyMOE/'))
 sys.path.insert(0, os.path.abspath('..')) 
 sys.path.insert(0, os.path.abspath('../..'))  
 # -- General configuration ---------------------------------------------------
