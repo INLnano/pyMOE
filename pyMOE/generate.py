1529d3dac315447a8759f9acd4099c0da236bc04
Diogo Aguiam <diogoaguiam@gmail.com>
2026-01-27 13:53:38 +0000

    removed matplotlib dependencies within `generate.py`

--- pyMOE/generate.py
+++ pyMOE/generate.py
@@ -14,7 +14,6 @@ from pyMOE.aperture import Aperture
 
 import numpy as np 
 from scipy import ndimage
-from matplotlib import pyplot as plt
 import math 
 import cv2 
 
@@ -570,56 +569,6 @@ def makegrid(N_pixels, xsize, ysize):
     
     return (XX, YY )
     
-##Code to create a gray scale with successive gray levels 
-def create_scale(N_pixels, nsz, ngs): 
-    """
-    returns a 2D array with a scale of successive gray levels 
-    
-    Args:
-        :N_pixels:  nr of pixels 
-        :nsz:       division in size 
-        :ngs:       nr of gray levels 
-    
-    """
-    
-    scale_img = np.zeros((N_pixels,N_pixels,3), np.uint8)
-
-    width = N_pixels 
-    height = N_pixels 
-    
-    nsz = N_pixels/ngs 
-    
-    xdims = np.arange(0,width, nsz)
-    
-    xdist = 255/ngs
-    xlevs = np.arange(0,255, xdist)
-    print(xlevs)
-
-    print(xdims)
-
-    gslev = ngs
-
-    xdimsint = np.array(xdims, dtype=int)
-    xdimsround = np.round(xdimsint)
-
-    for iw, wd in enumerate(xlevs):
-        if iw == (len(xdims)-1): 
-            break 
-            
-        gss =255 - np.round(wd)
-        
-        colorgray = np.uint8([[gss,gss,gss]])
-        
-        graycolor = cv2.cvtColor(colorgray, cv2.COLOR_GRAY2RGB)
-        
-        scale_img[:,xdimsround[iw]:xdimsround[iw+1]] = (int(graycolor[0][0][0]),int(graycolor[0][0][1]),int(graycolor[0][0][2]))      # (B, G, R)
-        
-    
-    fig1 = plt.figure()
-    plt.imshow(scale_img, vmin=0, vmax=255, cmap=plt.get_cmap("Greys"))
-    plt.title("scaled")
- 
-    return scale_img
     
 
 
